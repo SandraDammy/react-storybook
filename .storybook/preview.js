5b74b797bf566243a60e5044f5574b4cb7ccd7d3
OluwadamilolaOd <oluwadamilolaod@ha-shem.com>
2024-03-11 14:04:49 +0100

    styling decorator in story book

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,26 @@
 /* global styling */ 
-
 import '../src/App.css'
 
-/* global styling */ 
+/* styling decorator in story book */ 
+const styles = {
+  display: "flex",
+  flexDirection: "column",
+  maxHeight: "auto",
+  justifyContent: "flex-start",
+  alignContent: "flex-start",
+  flexWrap: "wrap",
+  height: "100%",
+  gap: "10px 30px"
+}
+
+export const decorators = [
+  (Story) => (
+    <div style={styles}>
+      <Story/>
+    </div>
+  )
+]
+
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
